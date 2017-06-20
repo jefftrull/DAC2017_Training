994b8dafb992b3fee9d07370c494d13bbb3657ba
Jeff Trull <jeff.trull@ciere.com>
2017-06-20 12:26:53 -0700

    Add simple generic visitor example

---
+++ visitor.cpp
@@ -0,0 +1,29 @@
+// Simple Visitor Pattern example
+
+class Instance;
+class Netlist;
+
+void DoNothingC(Instance *) {}
+
+void
+traverse_netlist_c(Netlist * n,
+                   void (*v)(Instance *)) {
+    // ...
+    // every time we encounter an instance, call the visitor
+    Instance * inst;
+    v(inst);           // always calls this pointer
+}
+
+struct DoNothingInstanceVisitor {
+    void operator()(Instance *) const {}
+};
+
+template<typename InstanceVisitor = DoNothingInstanceVisitor>
+void
+traverse_netlist(Netlist * n,
+                 InstanceVisitor v = InstanceVisitor()) {
+    // ...
+    // every time we encounter an instance, call the visitor
+    Instance * inst;  // = whatever
+    v(inst);          // optimized away for DoNothingInstanceVisitor
+}
